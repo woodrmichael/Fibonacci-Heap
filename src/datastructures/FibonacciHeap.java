1688f86c6d3b7990856a16da6554e3d61267a32f
woodrmichael <woodm@msoe.edu>
2024-11-11 19:08:13 -0600

    Method Stubs

--- src/datastructures/FibonacciHeap.java
+++ src/datastructures/FibonacciHeap.java
@@ -1,6 +1,7 @@
 package datastructures;
 
 public class FibonacciHeap<T extends Comparable<T>> {
+    private Node<T> minimum;
 
     private static final class Node<T> {
         private int key;
@@ -9,12 +10,49 @@ public class FibonacciHeap<T extends Comparable<T>> {
         private Node<T> right;
         private int degree;
 
-        public Node(int key, T value) {
+        public Node(T value, int key) {
             this.key = key;
             this.value = value;
             this.left = null;
             this.right = null;
             this.degree = 0;
         }
+    }
+
+
+    public FibonacciHeap() {
+
+    }
+
+    public void heapify() {
+
+    }
+
+    public void insert(T value, int key) {
+
+    }
+
+    public void union() {
+
+    }
+
+    public T minimum() {
+        return minimum.value;
+    }
+
+    public T extractMinimum() {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void decreaseKey(T value, int key) {
+
+    }
+
+    public void delete(T value) {
+
     }
 }
