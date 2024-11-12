953821d582519703543bc479bb4d2235a014cc64
woodrmichael <woodm@msoe.edu>
2024-11-11 18:13:07 -0600

    Node Class

--- src/datastructures/FibonacciHeap.java
+++ src/datastructures/FibonacciHeap.java
@@ -1,7 +1,20 @@
 package datastructures;
 
-public class FibonacciHeap {
-    private static final class Node {
+public class FibonacciHeap<T extends Comparable<T>> {
 
+    private static final class Node<T> {
+        private int key;
+        private T value;
+        private Node<T> left;
+        private Node<T> right;
+        private int degree;
+
+        public Node(int key, T value) {
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+            this.degree = 0;
+        }
     }
 }
